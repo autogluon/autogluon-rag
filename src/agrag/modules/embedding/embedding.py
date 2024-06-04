893db38eef1f286d6de54a83a95c57cb5ee6067e
Shreyash <shreyash2106@berkeley.edu>
2024-06-04 10:43:39 -0700

    add more configurability to module functions

--- src/agrag/modules/embedding/embedding.py
+++ src/agrag/modules/embedding/embedding.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Union
+from typing import Any, Dict, List, Union
 
 import torch
 from sentence_transformers import SentenceTransformer
@@ -12,19 +12,30 @@ logger = logging.getLogger("rag-logger")
 
 class EmbeddingModule:
     """
-    A class used to generate embeddings for text dat.
+    A class used to generate embeddings for text data from documents.
 
     Attributes:
     ----------
-    model_name : str
-        The name of the Huggingface model or SentenceTransformer to use for generating embeddings.
-    tokenizer : transformers.PreTrainedTokenizer
-        The tokenizer associated with the Huggingface model.
-    model : transformers.PreTrainedModel
-        The Huggingface model used for generating embeddings.
-    pooling_strategy : str
-        The strategy used for pooling embeddings. Options are 'average', 'max', 'cls'.
-        If no option is provided, will default to using no pooling method.
+    hf_model : str, optional
+        The name of the Huggingface model to use for generating embeddings (default is "BAAI/bge-large-en").
+    use_sentence_transf: bool, optional
+        Whether or not to use the SentenceTransformer model
+    st_model : str, optional
+        The name of the SentenceTransformer model to use for generating embeddings (default is "paraphrase-MiniLM-L6-v2").
+    pooling_strategy : str, optional
+        The strategy to use for pooling embeddings. Options are 'average', 'max', 'cls' (default is None).
+    hf_model_params : dict, optional
+        Additional parameters to pass to the Huggingface model's `from_pretrained` initializer method.
+    hf_tokenizer_init_params : dict, optional
+        Additional parameters to pass to the Huggingface tokenizer's `from_pretrained` initializer method.
+    st_params : dict, optional
+        Additional parameters to pass to the `SentenceTransformer` initializer method.
+    hf_tokenizer_params : dict, optional
+        Additional parameters to pass to the `tokenizer` method for the Huggingface model.
+    st_encode_params : dict, optional
+        Additional parameters to pass to the SentenceTransformer `encode` method.
+    hf_forward_params : dict, optional
+        Additional parameters to pass to the Huggingface model's `forward` method.
 
     Methods:
     -------
@@ -35,20 +46,33 @@ class EmbeddingModule:
     def __init__(
         self,
         hf_model: str = "BAAI/bge-large-en",
+        use_sentence_transf: bool = False,
         st_model: str = "paraphrase-MiniLM-L6-v2",
         pooling_strategy: str = None,
+        hf_model_params: Dict[str, Any] = None,
+        hf_tokenizer_init_params: Dict[str, Any] = None,
+        st_params: Dict[str, Any] = None,
+        hf_tokenizer_params: Dict[str, Any] = None,
+        st_encode_params: Dict[str, Any] = None,
+        hf_forward_params: Dict[str, Any] = None,
     ):
-        self.sentence_transf = False
+        self.use_sentence_transf = use_sentence_transf
         self.hf_model = hf_model
         self.st_model = st_model
-        if st_model == "sentence_transformer":
-            self.model = SentenceTransformer(self.st_model)
-            self.sentence_transf = True
+        self.hf_model_params = hf_model_params or {}
+        self.hf_tokenizer_init_params = hf_tokenizer_init_params or {}
+        self.st_params = st_params or {}
+        self.hf_tokenizer_params = hf_tokenizer_params or {}
+        self.st_encode_params = st_encode_params or {}
+        self.hf_forward_params = hf_forward_params or {}
+        if self.use_sentence_transf:
+            self.model = SentenceTransformer(self.st_model, **self.st_params)
+            self.use_sentence_transf = True
         else:
             logger.info(f"Default to using Huggingface since no model was provided.")
             logger.info(f"Using Huggingface Model: {self.hf_model}")
-            self.tokenizer = AutoTokenizer.from_pretrained(self.hf_model)
-            self.model = AutoModel.from_pretrained(self.hf_model)
+            self.tokenizer = AutoTokenizer.from_pretrained(self.hf_model, **self.hf_tokenizer_init_params)
+            self.model = AutoModel.from_pretrained(self.hf_model, **self.hf_model_params)
         self.pooling_strategy = pooling_strategy
 
     def create_embeddings(self, data: List[str]) -> Union[List[torch.Tensor], torch.Tensor]:
@@ -66,15 +90,15 @@ class EmbeddingModule:
             A list of embeddings corresponding to the input data chunks if pooling_strategy is 'none',
             otherwise a single tensor with the pooled embeddings.
         """
-        if self.sentence_transf:
-            embeddings = self.model.encode(data, convert_to_tensor=True)
+        if self.use_sentence_transf:
+            embeddings = self.model.encode(data, convert_to_tensor=True, **self.st_encode_params)
             embeddings = pool(embeddings, self.pooling_strategy)
         else:
             embeddings = []
             for text in data:
-                inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+                inputs = self.tokenizer(text, return_tensors="pt", **self.hf_tokenizer_params)
                 with torch.no_grad():
-                    outputs = self.model(**inputs)
+                    outputs = self.model(**inputs, **self.hf_forward_params)
                 embedding = pool(outputs.last_hidden_state, self.pooling_strategy)
                 embeddings.append(embedding)
         if not self.pooling_strategy:
--- tests/unittests/test_embedding.py
+++ tests/unittests/test_embedding.py
@@ -17,9 +17,21 @@ class TestEmbeddingModule(unittest.TestCase):
         mock_tokenizer.return_value = self.mock_tokenizer
         mock_model.return_value = self.mock_model
 
-        self.embedding_module = EmbeddingModule(hf_model="some-model", pooling_strategy=None)
+        hf_model_params = {"param": "param"}
+        hf_tokenizer_params = {"param": True}
+        tokenizer_params = {"padding": 10, "max_length": 512}
+        forward_params = {"param": True}
 
-    def test_create_embeddings_no_pooling(self):
+        self.embedding_module = EmbeddingModule(
+            hf_model="some-model",
+            pooling_strategy=None,
+            hf_model_params=hf_model_params,
+            hf_tokenizer_init_params=hf_tokenizer_params,
+            hf_tokenizer_params=tokenizer_params,
+            hf_forward_params=forward_params,
+        )
+
+    def test_create_embeddings_hf_no_pooling(self):
         self.mock_tokenizer.return_tensors.return_value = {
             "input_ids": torch.tensor([[1, 2, 3], [4, 5, 6]]),
             "attention_mask": torch.tensor([[1, 1, 1], [1, 1, 1]]),
@@ -27,6 +39,7 @@ class TestEmbeddingModule(unittest.TestCase):
         self.mock_model.return_value = MagicMock(last_hidden_state=torch.rand((10, 20, 100)))
 
         data = ["test sentence 1", "test sentence 2"]
+        self.embedding_module.use_sentence_transf = False
         embeddings = self.embedding_module.create_embeddings(data)
 
         self.assertEqual(len(embeddings), 2)
@@ -38,8 +51,17 @@ class TestEmbeddingModule(unittest.TestCase):
         mock_model.encode.return_value = torch.rand((2, 100))
         mock_sentence_transformer.return_value = mock_model
 
+        st_params = {"param": "param"}
+        encode_params = {"batch_size": 32}
+
         with patch("agrag.modules.embedding.embedding.SentenceTransformer", return_value=mock_model):
-            self.embedding_module = EmbeddingModule(st_model="sentence_transformer", pooling_strategy=None)
+            self.embedding_module = EmbeddingModule(
+                st_model="sentence_transformer",
+                pooling_strategy=None,
+                st_params=st_params,
+                st_encode_params=encode_params,
+                use_sentence_transf=True,
+            )
 
             data = ["test sentence 1", "test sentence 2"]
             embeddings = self.embedding_module.create_embeddings(data)
