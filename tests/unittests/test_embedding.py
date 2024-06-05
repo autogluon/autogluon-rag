0cc1a5df5c1ff4bc018c021a3cfc8241be792c8d
Shreyash Sridhar Iyengar <54525027+shreyash2106@users.noreply.github.com>
2024-06-05 14:29:45 -0700

    Embedding Module (#6)
    * add support for SentenceTransformer
    
    * merge from dev branch
    
    * modify requirements
    
    * move defaults file into configs folder
    
    * modify gitignore
    
    * add more configurability to module functions
    
    * modify args docstring
    
    * change average pooling to mean pooling
    
    * use only huggingface
    
    * normalize util fucntion
    
    * modify default model
    
    * use model instead of data for config file
    
    * use multi GPU DataParallel
    
    * remove space
    
    * default values for normalization
    
    * formatting
    
    * unittest for normalization
    
    * normalization params
    
    * update docstring
    
    * add check for data parallel usage
    
    * use encode instead of create_embeddings

---
+++ tests/unittests/test_embedding.py
@@ -0,0 +1,84 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+import torch
+
+from agrag.modules.embedding.embedding import EmbeddingModule
+from agrag.modules.embedding.utils import normalize_embedding, pool
+
+
+class TestEmbeddingModule(unittest.TestCase):
+    @patch("agrag.modules.embedding.embedding.AutoTokenizer.from_pretrained")
+    @patch("agrag.modules.embedding.embedding.AutoModel.from_pretrained")
+    def setUp(self, mock_model, mock_tokenizer):
+        self.mock_tokenizer = MagicMock()
+        self.mock_model = MagicMock()
+        mock_tokenizer.return_value = self.mock_tokenizer
+        mock_model.return_value = self.mock_model
+
+        hf_model_params = {"param": "param"}
+        hf_tokenizer_params = {"param": True}
+        tokenizer_params = {"padding": 10, "max_length": 512}
+        forward_params = {"param": True}
+
+        self.embedding_module = EmbeddingModule(
+            hf_model="some-model",
+            pooling_strategy=None,
+            hf_model_params=hf_model_params,
+            hf_tokenizer_init_params=hf_tokenizer_params,
+            hf_tokenizer_params=tokenizer_params,
+            hf_forward_params=forward_params,
+        )
+
+    def test_encode_hf(self):
+        self.mock_tokenizer.return_tensors.return_value = {
+            "input_ids": torch.tensor([[1, 2, 3], [4, 5, 6]]),
+            "attention_mask": torch.tensor([[1, 1, 1], [1, 1, 1]]),
+        }
+        self.mock_model.return_value = MagicMock(last_hidden_state=torch.rand((10, 20, 100)))
+
+        data = ["test sentence 1", "test sentence 2"]
+        embeddings = self.embedding_module.encode(data)
+
+        self.assertEqual(len(embeddings), 2)
+        self.assertTrue(all(isinstance(embedding, torch.Tensor) for embedding in embeddings))
+
+    @patch("agrag.modules.embedding.embedding.AutoModel.from_pretrained")
+    def test_pool_mean(self, mock_model):
+        self.embedding_module.pooling_strategy = "mean"
+        mock_model.return_value = MagicMock(last_hidden_state=torch.rand((10, 20, 100)))
+        embeddings = torch.rand((10, 20, 100))
+        pooled_embeddings = pool(embeddings, self.embedding_module.pooling_strategy)
+
+        self.assertEqual(pooled_embeddings.shape, (10, 100))
+
+    @patch("agrag.modules.embedding.embedding.AutoModel.from_pretrained")
+    def test_pool_max(self, mock_model):
+        self.embedding_module.pooling_strategy = "max"
+        mock_model.return_value = MagicMock(last_hidden_state=torch.rand((10, 20, 100)))
+        embeddings = torch.rand((10, 20, 100))
+        pooled_embeddings = pool(embeddings, self.embedding_module.pooling_strategy)
+
+        self.assertEqual(pooled_embeddings.shape, (10, 100))
+
+    @patch("agrag.modules.embedding.embedding.AutoModel.from_pretrained")
+    def test_pool_cls(self, mock_model):
+        self.embedding_module.pooling_strategy = "cls"
+        mock_model.return_value = MagicMock(last_hidden_state=torch.rand((10, 20, 100)))
+        embeddings = torch.rand((10, 20, 100))
+        pooled_embeddings = pool(embeddings, self.embedding_module.pooling_strategy)
+
+        self.assertEqual(pooled_embeddings.shape, (10, 100))
+
+    def test_normalize_embedding(self):
+        embedding = torch.rand((10, 100))
+        normalized_embedding = normalize_embedding(embedding, {})
+
+        expected_norm = torch.ones((10,))
+        actual_norm = torch.norm(normalized_embedding, p=2, dim=1)
+
+        self.assertTrue(torch.allclose(expected_norm, actual_norm, atol=1e-6))
+
+
+if __name__ == "__main__":
+    unittest.main()
