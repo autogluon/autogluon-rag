be9b1ffd40e4dc2935f2d898710ffbaab7ae51a4
Shreyash <shreyash2106@berkeley.edu>
2024-06-06 16:07:12 -0700

    READ ME for configurable parameters

--- src/agrag/modules/vector_db/vector_database.py
+++ src/agrag/modules/vector_db/vector_database.py
@@ -25,7 +25,7 @@ class VectorDatabaseModule:
     similarity_threshold : float
         The threshold for considering embeddings as duplicates based on cosine similarity (default is 0.95)
     similarity_fn : str
-        The name of the similarity function for determining duplicate embeddings. Options are 'cosine', 'euclidean', 'manhattan' (default is 'cosine').
+        The similarity function used for determining similarity scores for embeddings. Options are 'cosine', 'euclidean', 'manhattan' (default is 'cosine').
 
     Methods:
     -------
