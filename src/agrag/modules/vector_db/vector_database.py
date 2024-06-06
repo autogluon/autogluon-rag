a0a31c4babfe42f5bb30ed746b024be3fc014a87
Shreyash <shreyash2106@berkeley.edu>
2024-06-06 11:59:08 -0700

    add folder for faiss

--- src/agrag/modules/vector_db/vector_database.py
+++ src/agrag/modules/vector_db/vector_database.py
@@ -4,7 +4,7 @@ from typing import Any, List, Union
 import faiss
 import torch
 
-from agrag.modules.vector_db.faiss import construct_faiss_index
+from agrag.modules.vector_db.faiss.faiss import construct_faiss_index
 from agrag.modules.vector_db.utils import pad_embeddings, remove_duplicates
 
 logger = logging.getLogger("rag-logger")
