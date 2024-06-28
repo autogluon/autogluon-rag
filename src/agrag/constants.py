0cb7a3800b07de93c1ad8bb5137ece5d4cc74fee
Shreyash <shreyash2106@berkeley.edu>
2024-06-28 10:59:29 -0700

    modify list of supported files

--- src/agrag/constants.py
+++ src/agrag/constants.py
@@ -2,4 +2,4 @@ DOC_ID_KEY = "doc_id"
 CHUNK_ID_KEY = "chunk_id"
 DOC_TEXT_KEY = "text"
 EMBEDDING_KEY = "embedding"
-SUPPORTED_FILE_EXTENSIONS = [".pdf", ".txt", ".docx", ".doc", ".rtf", ".csv", ".md", ".py"]
+SUPPORTED_FILE_EXTENSIONS = [".pdf", ".txt", ".docx", ".doc", ".rtf", ".csv", ".md", ".py", ".log"]
