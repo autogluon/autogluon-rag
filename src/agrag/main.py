4c9be1cf6e5f8833c483c1123da8a1342d284cc9
Shreyash <shreyash2106@berkeley.edu>
2024-07-03 17:54:34 -0700

    move while loop into generate_responses

--- src/agrag/main.py
+++ src/agrag/main.py
@@ -4,14 +4,7 @@ from agrag.agrag import AutoGluonRAG
 def ag_rag():
     agrag = AutoGluonRAG(preset_quality="medium", data_dir="s3://autogluon-rag-github-dev/autogluon_docs/")
     agrag.initialize_rag_pipeline()
-    while True:
-        query_text = input(
-            "Please enter a query for your RAG pipeline, based on the documents you provided (type 'q' to quit): "
-        )
-        if query_text == "q":
-            break
-
-        response = agrag.generate_response(query=query_text)
+    agrag.generate_responses()
 
 
 if __name__ == "__main__":
