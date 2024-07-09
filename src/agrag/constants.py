25b8494e04effd4609b48967cfc2ecddeb78e3d3
Shreyash <shreyash2106@berkeley.edu>
2024-07-09 11:06:49 -0700

    same embedding dimensions in metadata

--- src/agrag/constants.py
+++ src/agrag/constants.py
@@ -2,4 +2,5 @@ DOC_ID_KEY = "doc_id"
 CHUNK_ID_KEY = "chunk_id"
 DOC_TEXT_KEY = "text"
 EMBEDDING_KEY = "embedding"
+EMBEDDING_DIM_KEY = "embedding_dim"
 SUPPORTED_FILE_EXTENSIONS = [".pdf", ".txt", ".docx", ".doc", ".rtf", ".csv", ".md", ".py", ".log"]
