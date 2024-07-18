8eca4dbb809656a793de055ab0871b301c45838d
Shreyash <shreyash2106@berkeley.edu>
2024-07-18 12:10:54 -0700

    working evaluation script with bert score

--- src/agrag/modules/evaluation/evaluate_agrag.py
+++ src/agrag/modules/evaluation/evaluate_agrag.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import evaluate
+import numpy as np
 from bs4 import BeautifulSoup
 from datasets import load_dataset
 
@@ -13,7 +14,7 @@ logger = logging.getLogger(__name__)
 # Load the Google Natural Questions dataset
 dataset = load_dataset("google-research-datasets/natural_questions", "dev")
 MAX_FILE_SIZE = 50 * 1000 * 1000  # 50 MB
-bleu_metric = evaluate.load("bleu")
+bertscore = evaluate.load("bertscore")
 
 # Create evaluation directory
 evaluation_dir = "evaluation_data"
@@ -58,7 +59,6 @@ def evaluate_responses(dataset, agrag):
     references = []
     predictions = []
 
-    max = 0
     for row in dataset["validation"]:
         query = row["question"]["text"]
         short_answer = row["annotations"]["short_answers"]
@@ -70,6 +70,9 @@ def evaluate_responses(dataset, agrag):
                 expected_responses += [text[0]]
         print(expected_responses)
 
+        if not expected_responses:
+            continue
+
         generated_response = agrag.generate_response(query)
 
         references.append(expected_responses)
@@ -79,19 +82,21 @@ def evaluate_responses(dataset, agrag):
         logger.info(f"Expected: {expected_responses}")
         logger.info(f"Generated: {generated_response}")
 
-        if max == 5:
-            break
-        max += 1
-
-    bleu_score = bleu_metric.compute(predictions=predictions, references=references)
-    logger.info(f"BLEU Score: {bleu_score}")
-
-    return bleu_score
+    print("Predictions: ", predictions)
+    print("References: ", references)
+    result = bertscore.compute(predictions=predictions, references=references, lang="en")
+    logger.info(f"Average BERTScore Precision: {np.mean(result['precision'])}")
+    logger.info(f"Average BERTScore Recall: {np.mean(result['recall'])}")
+    logger.info(f"Average BERTScore F1 Score: {np.mean(result['f1'])}")
 
 
 if __name__ == "__main__":
-    # save_documents_to_files(dataset, evaluation_dir)
+    save_documents_to_files(dataset, evaluation_dir)
     agrag = AutoGluonRAG(preset_quality="medium_quality", data_dir=evaluation_dir)
-    agrag.args.generator_query_prefix = "You will be answering questions based on Wikipedia articles. Your response must only contain the actual answer and no extra information or words. Keep it as short as possible. Your answer may even be a singular word or number."
+    agrag.args.generator_query_prefix = (
+        "You will be answering questions based on Wikipedia articles. "
+        "Your response must contain only the actual answer itself and no extra information or words. Provide just the answer instead of forming full sentences. "
+        "Your answer may even be a singular word or number. For example, if the query is \"what percentage of the earth's surface is water?\", your response must be 'roughly 78%', or '78%'. "
+    )
     agrag.initialize_rag_pipeline()
     evaluate_responses(dataset, agrag)
