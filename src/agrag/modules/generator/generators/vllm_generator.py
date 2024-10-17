e1d66a5772de283a5d088b3199ef27fab8343373
Shuai Zhang <cheungdaven@gmail.com>
2024-10-17 13:25:38 -0700

    Enable vLLM Initialization Parameters (#81)
    * support more faiss index algorithms and refactor the interface
    
    * fix lint errors
    
    * fix lint errors
    
    * fix lint errors
    
    * fix lint errors
    
    * fix lint errors
    
    * Add vllm_init_params
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-3-59.us-west-2.compute.internal>

--- src/agrag/modules/generator/generators/vllm_generator.py
+++ src/agrag/modules/generator/generators/vllm_generator.py
@@ -22,6 +22,9 @@ class VLLMGenerator:
     ----------
     model_name : str
         The name of the vLLM model to use for response generation.
+    vllm_init_params : Dict,
+        Parameters to initialize vllm, including model configs and vllm engine configs.
+        See https://docs.vllm.ai/en/latest/dev/offline_inference/llm.html for more details.
     sampling_params: SamplingParams
         The sampling parameters for vLLM.
 
@@ -34,17 +37,18 @@ class VLLMGenerator:
     def __init__(
         self,
         model_name: str,
+        vllm_init_params: Dict = None,
         vllm_sampling_params: Dict = None,
     ):
         self.model_name = model_name
 
         if found:
             self.sampling_params = SamplingParams(**vllm_sampling_params) if vllm_sampling_params else SamplingParams()
-            self.llm = LLM(model=self.model_name)
+            self.llm = LLM(model=self.model_name, **vllm_init_params)
 
             logger.info(f"Using vLLM Model {self.model_name} for VLLM Generator")
         else:
-            logger.warning("vLLM is not supported on this message")
+            logger.warning("vLLM is not supported on this message. Please install vllm.")
 
     def generate_response(self, query: str) -> str:
         """
